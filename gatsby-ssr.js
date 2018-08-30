c4d1c9f4c42da7f5ec7d89a5ef29ebd1432ed76a
Jonathan Ganc <jonganc@gmail.com>
2018-08-30 17:07:39 -0400

    Just have MUIRoot component

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,6 +10,36 @@ import getMuiContext from './src/common/getMuiContext';
 // // redux: from https://github.com/gatsbyjs/gatsby/blob/master/examples/using-redux/gatsby-ssr.js
 // export const wrapRootElement = wrapWithProvider;
 
+// Get the context of the page to collected side effects.
+const muiContext = getMuiContext();
+
+export const wrapRootElement = ({ element }) => {
+  const bodyHtml = renderToString(
+    <JssProvider
+      registry={muiContext.sheetsRegistry}
+      generateClassName={muiContext.generateClassName}
+    >
+      {React.cloneElement(bodyComponent, {
+        muiContext,
+      })}
+    </JssProvider>,
+  );
+
+  replaceBodyHTMLString(bodyHtml);
+
+  setHeadComponents([
+    <style
+      type="text/css"
+      id="server-side-jss"
+      key="server-side-jss"
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{
+        __html: muiContext.sheetsRegistry.toString(),
+      }}
+    />,
+  ]);
+};
+
 // material UI: from https://github.com/mui-org/material-ui/tree/master/examples/gatsby
 export const replaceRenderer = ({
   bodyComponent,
